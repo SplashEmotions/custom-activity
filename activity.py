d0809d23bdf49d3cde8146fc633eebcfb9938d4a
SplashEmotions <smertvam6@gmail.com>
2023-05-14 23:42:04 +0600

    first

---
+++ activity.py
@@ -0,0 +1,21 @@
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import discord
+
+Client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print("Установка активности!") # увед. о начале работы
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}!")
+    activity = discord.Game("шо случиас") # суда название активности
+    await bot.change_presence(activity=activity)
+    print("Активность установлена") # уведомление о установлении активности
+
+bot.run("моего тут не будет") #суда ваш токен
